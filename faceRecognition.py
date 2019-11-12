c1d226fa3cb89b517b0dce83d90ed7f055d16077
nixce29 <53696374+nixce29@users.noreply.github.com>
2019-11-12 19:14:37 +0700

    Commit Demo FaceRecognition V.2

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,10 +1,48 @@
 import cv2
 import os
-import numpy as numpy
+import numpy as np
 
 def faceDetection(test_img):
     gray_img=cv2.cvtColor(test_img,cv2.COLOR_BGR2GRAY)
     face_haar_cascade=cv2.CascadeClassifier('D:/FaceRecognition/Haarcascade/haarcascade_frontalface_default.xml')
-    faces=face_haar_cascade.detectMultiScale(gray_img,scaleFactor=1.32,minNeighbors=5)
+    faces=face_haar_cascade.detectMultiScale(gray_img,scaleFactor=1.3,minNeighbors=5)
 
     return faces,gray_img
+def label_for_training_data(directory):
+    faces=[]
+    faceID=[]
+
+    for path,subdirnames,filename in os.walk(directory):
+        for filename in filename:
+            if filename.startswith("."):
+                print("Skipping system file")
+                continue
+            
+            id=os.path.basename(path)
+            img_path=os.path.join(path,filename)
+            print("img_path",img_path)
+            print("id: ",id)
+            test_img=cv2.imread(img_path)
+            if test_img is None:
+                print("Image not loaded properly")
+                continue
+            faces_rect,gray_img=faceDetection(test_img)
+            if len(faces_rect)!=1:
+                continue #Since we are assuming only single person images are being fed to classifier
+            (x,y,w,h)=faces_rect[0]
+            roi_gray=gray_img[y:y+w,x:x+h]
+            faces.append(roi_gray)
+            faceID.append(int(id))
+    return faces,faceID
+
+def train_classifier(faces,faceID):
+    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+    face_recognizer.train(faces,np.array(faceID))
+    return face_recognizer
+
+def darw_rect(test_img,face):
+    (x,y,w,h)=face
+    cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=5)
+
+def put_text(test_img,text,x,y):
+    cv2.putText(test_img,text,(x,y),cv2.FONT_HERSHEY_DUPLEX,5,(255,0,0),6)
--- tester.py
+++ tester.py
@@ -3,14 +3,34 @@ import os
 import numpy as ny
 import faceRecognition as fr
 
-test_img=cv2.imread('D:/FaceRecognition/TestImages/pai2.jpg')
+test_img=cv2.imread('D:/FaceRecognition/TestImages/testimage.jpg')
 faces_detected,gray_img=fr.faceDetection(test_img)
 print("faces_detested:",faces_detected)
 
-for (x,y,w,h) in faces_detected:
-    cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=5)
+#for (x,y,w,h) in faces_detected:
+#    cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=5)
 
-resized_img=cv2.resize(test_img,(960,636))
+#resized_img=cv2.resize(test_img,(1000,700))
+#cv2.imshow("face detection tutorial",resized_img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows
+
+faces,faceID=fr.label_for_training_data('D:/FaceRecognition/trainingimages')
+face_recognizer=fr.train_classifier(faces,faceID)
+face_recognizer.save('trainingData.yml')
+name={0:'Pai',1:'Nice'}
+
+for face in faces_detected:
+    (x,y,w,h)=face
+    roi_gray=gray_img[y:y+h,x:x+h]
+    label,confidence=face_recognizer.predict(roi_gray)
+    print('confidence: ',confidence)
+    print('label: ',label)
+    fr.darw_rect(test_img,face)
+    predicted_name=name[label]
+    fr.put_text(test_img,predicted_name,x,y)
+
+resized_img=cv2.resize(test_img,(1000,700))
 cv2.imshow("face detection tutorial",resized_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows
